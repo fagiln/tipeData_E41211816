c3c42b29af34959288b04dbd5339577141507cd9
Fagil Nuril Akbar <fagilnuril18@gmail.com>
2022-09-20 12:20:18 +0700

    done

---
+++ tipedata/bin/looping.dart
@@ -0,0 +1,11 @@
+void main(List<String> arg) {
+  print("LOOPING PERTAMA");
+  for (var i = 1; i <= 21; i += 2) {
+    print("$i - SAYA CINTA CODING");
+  }
+  print("\n");
+  print("LOOPING KEDUA");
+  for (var i = 30; i >= 3; i -= 3) {
+    print("$i - SAYA AKAN MENJADI SEORANG PROGRAMER");
+  }
+}
--- tipedata/bin/werewolf.dart
+++ tipedata/bin/werewolf.dart
@@ -1,28 +1,43 @@
 import 'dart:io';
 
 void main(List<String> arg) {
-  var char_list = ["werewolf" ,"witch", "guardian", "villager"];
+  String nama;
   print("SELAMAT DATANG DI PERMAINAN WEREWOLF 2022");
-  print("SILAHKAN PILIH SALAH SATU KARAKTER ANDA");
-  // print();
-  stdout.writeln("silahkan tulis karakter yang anda inginkan atau ketik 'info' untuk mengetahui skill setiap karakter");
+
+while(true){
+  stdout.writeln('Masukkan nama anda ');
+  nama = stdin.readLineSync()!;
+    if (nama != "") {
+      print("Halo $nama silahkan pilih salah satu karakter anda");
+      break;
+    } else {
+      print("Nama tidak boleh kosong !");
+    }
+}
+print("Nama - nama karakter :");
+print("- werewolf\n- witch\n- guardian\n- villager");
+  stdout.writeln(
+      "Silahkan tulis karakter yang anda inginkan atau ketik 'info' untuk mengetahui skill setiap karakter");
   String input = stdin.readLineSync()!;
-  if (input == "werewolf") {
+  if (input == "") {
     print(
-        "Karakter yang anda pilih adalah Werewolf, Silahkan memangsa villager dan jangan sampai ketahuan");
-  } else if (input == "witch") {
-    print("Karakter yang kamu pilih adalah Witch, Gunakan kekuatan magic kamu untuk mengetahui siapa Werewolf nya");
-  }
-  else if (input == "guardian"){
-    print("Karakter yang kamu pilih adalah Guardian, Selamatkan warga ketika Werewolf menyerang");
-  }
-  else if (input== "villager"){
-    print("Karakter yang kamu pilih adalah Villager, Kabur lah ketika kamu mendengar suara dari Werewolf");
-  }
-  else if (input == "info"){
-    print("- Werewolf = Dapat memangsa Villager dan Witch, tetapi dapat di bunuh oleh Guardian\n- Witch = Dapat meramal Werewolf yang menyamar menjadi Villager\n- Guardian = Dapat melindungi Villager dan membunuh Werewolf\n- Villager = Hanya warga desa yang tidak bisa melindungi diri");
-  }
-  else {
-    print("Karakter tidak tersedia atau coba cek penulisan kamu, harus huruf kecil semua yah :) ");
+        "Halo $nama, kamu belum memilih karakter, silahkan pilih karaktermu ");
+  } else if (nama != "" && input == "werewolf") {
+    print(
+        "Halo Werewolf $nama, Silahkan memangsa villager dan jangan sampai ketahuan");
+  } else if (nama != "" && input == "witch") {
+    print(
+        "Halo Witch $nama, Gunakan kekuatan magic kamu untuk mengetahui siapa Werewolf nya");
+  } else if (nama != "" && input == "guardian") {
+    print("Halo Guardian $nama, Selamatkan warga ketika Werewolf menyerang");
+  } else if (nama != "" && input == "villager") {
+    print(
+        "Halo Villager $nama, Kabur lah ketika kamu mendengar suara dari Werewolf");
+  } else if (input == "info") {
+    print(
+        "- Werewolf = Dapat memangsa Villager dan Witch, tetapi dapat di bunuh oleh Guardian\n- Witch = Dapat meramal Werewolf yang menyamar menjadi Villager\n- Guardian = Dapat melindungi Villager dan membunuh Werewolf\n- Villager = Hanya warga desa yang tidak bisa melindungi diri");
+  } else {
+    print(
+        "Karakter tidak tersedia atau coba cek penulisan kamu, harus huruf kecil semua yah :) ");
   }
 }
